6735d9c4b8d62693365fbe17e2efb6754f977b4b
Viknash <viknash@hobbiz.com>
2015-05-02 00:14:43 +0800

    Fixed tests

--- app.js
+++ app.js
@@ -1,36 +1,44 @@
-var utils        = require('utils')
-  , chalk        = require('chalk')
-  , env          = utils.bootstrapEnv()
-  , app          = env.app
-  , debug        = env.debug
-  , helpers      = utils.helpers
-  , bodyParser   = require('body-parser')
-  , moduleLoader = env.moduleLoader;
+var path = require('path'),
+    utilsPath = path.resolve(path.join(__dirname, 'lib', 'utils'));
+
+var utils = require(utilsPath),
+    chalk = require('chalk'),
+    env = utils.bootstrapEnv(),
+    app = env.app,
+    debug = require('debug')('cleverstack:app'),
+    helpers = utils.helpers,
+    bodyParser = require('body-parser'),
+    moduleLoader = env.moduleLoader;
+
+debug.log = console.log.bind(console);
+require('debug').log = console.info.bind(console);
 
 moduleLoader.on('beforeLoad', function setupExpress() {
-  debug('Configuring express application...');
+    debug('Configuring express application...');
 
-  app.use(require('morgan')('dev'));
-  app.use(require('response-time')());
-  app.use(require('method-override')());
-  app.use(require('cors')(env.config.cors));
-  app.use(require('compression')());
+    app.use(require('morgan')('dev'));
+    app.use(require('response-time')());
+    app.use(require('method-override')());
+    app.use(require('cors')(env.config.cors));
+    app.use(require('compression')());
 
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
 });
 
 moduleLoader.on('modulesLoaded', function initializeRoutes() {
-  debug('Initializing routes...');
-  moduleLoader.initializeRoutes();
+    debug('Initializing routes...');
+    moduleLoader.initializeRoutes();
 });
 
 moduleLoader.on('routesInitialized', function startApp() {
-  debug('Starting http server...');
+    debug('Starting http server...');
 
-  app.listen(env.webPort, function appReady() {
-    debug('Listening on port %s', chalk.yellow(env.webPort));
-  });
+    app.listen(env.webPort, function appReady() {
+        debug('Listening on port %s', chalk.yellow(env.webPort));
+    });
 });
 
 debug('Loading %s hook files...', chalk.yellow('app.js'));
--- cluster.js
+++ cluster.js
@@ -1,35 +1,36 @@
-var os      = require('os')
-  , path    = require('path')
-  , chalk   = require('chalk')
-  , cluster = require('cluster')
-  , debug   = require('debug')(cluster.isMaster ? 'cleverstack:cluster' : 'cleverstack:app')
-  , config  = require(path.resolve(path.join(__dirname, 'config')))
-  , helpers = require(path.resolve(path.join(__dirname, 'lib', 'utils', 'helpers.js')));
+var os = require('os'),
+    path = require('path'),
+    chalk = require('chalk'),
+    cluster = require('cluster'),
+    debug = require('debug')(cluster.isMaster ? 'cleverstack:cluster' : 'cleverstack:app'),
+    config = require(path.resolve(path.join(__dirname, 'config'))),
+    helpers = require(path.resolve(path.join(__dirname, 'lib', 'utils', 'helpers.js')));
 
 // Clean and set the NODE_PATH for magic modules
 process.env.NODE_PATH = helpers.nodePath();
+require('debug').log = console.info.bind(console);
 
 debug('started with pid %s', chalk.yellow(process.pid));
 debug('%s is "%s"', chalk.magenta('NODE_PATH'), chalk.yellow(process.env.NODE_PATH));
 
 if (!cluster.isMaster) {
-  
-  // Worker Process, load app.js
-  require(path.join(__dirname, 'app.js'));
+
+    // Worker Process, load app.js
+    require(path.join(__dirname, 'app.js'));
 } else {
 
-  // Master Process, setup cluster
-  debug('Loading %s hook files...', chalk.yellow('cluster.js'));
-  helpers.loadModulesFileByName('cluster.js', config, cluster, debug);
+    // Master Process, setup cluster
+    debug('Loading %s hook files...', chalk.yellow('cluster.js'));
+    helpers.loadModulesFileByName('cluster.js', config, cluster, debug);
 
-  // Spawn new app process when a child dies
-  cluster.on('exit', function(worker, code, signal) {
-    debug('Worker %s has died with code %s and signal %s - Forking new process in 2.5 seconds...', worker.pid, code, signal);
-    setTimeout(cluster.fork.bind(cluster), 2500);
-  });
+    // Spawn new app process when a child dies
+    cluster.on('exit', function (worker, code, signal) {
+        debug('Worker %s has died with code %s and signal %s - Forking new process in 2.5 seconds...', worker.pid, code, signal);
+        setTimeout(cluster.fork.bind(cluster), 2500);
+    });
 
-  // Spawn app processes
-  for (var i = 0; i < (config.numChildren ? config.numChildren : os.cpus()); ++i) {
-    cluster.fork();
-  }
+    // Spawn app processes
+    for (var i = 0; i < (config.numChildren ? config.numChildren : os.cpus()); ++i) {
+        cluster.fork();
+    }
 }
