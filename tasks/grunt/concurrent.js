a81da91fd44836c5b62b0379cb4bb1def36e7931
Viknash <viknash@hobbiz.com>
2015-05-02 01:50:08 +0800

    Test Fix

--- tasks/grunt/concurrent.js
+++ tasks/grunt/concurrent.js
@@ -1,24 +1,29 @@
 module.exports = {
-  config: {
-    servers: {
-      tasks: ['watch:schema', 'nodemon:cluster'],
-      options: {
-        logConcurrentOutput: true
-      }
+    config: {
+        servers: {
+            tasks: ['watch:schema', 'nodemon:cluster'],
+            options: {
+                logConcurrentOutput: true
+            }
+        },
+        debug: {
+            tasks: ['exec:debugApp', 'node-inspector:app'],
+            options: {
+                logConcurrentOutput: true
+            }
+        },
+        dummy: {}
     },
-    debug: {
-      tasks: ['exec:debugApp', 'node-inspector:app'],
-      options: {
-        logConcurrentOutput: true
-      }
+    register: function (grunt) {
+        grunt.registerTask('server', ['concurrent:servers']);
+        grunt.registerTask('server:debug', ['concurrent:debug']);
+        grunt.registerTask('server:web', ['server']);
+        grunt.registerTask('serve:debug', ['server:debug']);
+        grunt.registerTask('serve', ['server:web']);
+        if (process.env.NODE_ENV.toUpperCase() !== 'TEST') {
+            grunt.registerTask('default', ['serve']);
+        } else {
+            grunt.registerTask('default', ['concurrent:dummy']);
+        }
     }
-  },
-  register: function(grunt) {
-    grunt.registerTask('server',       ['concurrent:servers']);
-    grunt.registerTask('server:debug', ['concurrent:debug']);
-    grunt.registerTask('server:web',   ['server']);
-    grunt.registerTask('serve:debug',  ['server:debug']);
-    grunt.registerTask('serve',        ['server:web']);
-    grunt.registerTask('default',      ['serve']);
-  }
 };
